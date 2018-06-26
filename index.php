cb315f4b351417ec80c6314e4921f0aaae162559
Unknown <benjamin.humblot@hetic.net>
2018-06-26 10:10:28 +0200

    initialize structure

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo 'test Phpok';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>webDoc</title>
+</head>
+<body>
+
+<h1>setUp contenu</h1>
+    
+</body>
+</html>
