fbb49af114aab75b5ce2136c3962d6be26593871
Sabrina Nedjah <snedjah78@gmail.com>
2018-06-27 16:35:48 +0200

    update link homepage

--- app/pages/chapter1.php
+++
@@ -1,6 +0,0 @@
-
-<div class="container">
-  <div class="row">
-      <div class="chapter_indicator">01</div>
-  </div>
-</div>
--- app/pages/home.php
+++ app/pages/home.php
@@ -2,6 +2,6 @@
     <div class="contener_intro">
         <p class="title_home">La culture en période de guerre, une bombe à retardement</p>
         <p class="descript_home">« En période de conflit, les belligérants arment la culture. Ils l’utilisent telle une bombe à retardement dont ils ont réglé l’heure et le jour d’explosion.  Le patrimoine culturel n’est pas seulement matériel. Il se compose d’une part d’intangible et de symbolique. »</p>
-        <p><button class="button_home"> <a href="chapter1">get started</a></button></p>
+        <p><button class="button_home"> <a href="pages/chapter1">get started</a></button></p>
     </div>
 </div>
