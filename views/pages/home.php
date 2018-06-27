f60efd544b2b96b213a6a5ce71875668ead372c2
Aurélien <aurelien.combes@hetic.net>
2018-06-27 10:59:34 +0200

    front rework

--- views/pages/home.php
+++ views/pages/home.php
@@ -5,41 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="assets/img/favicon.ico">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png"/>
 
     <title>HERITAGE EMOTIONS</title>
 
     <link href="assets/styles/reset.css" rel="stylesheet">
-    <link href="vendor/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/materialize.min.css" rel="stylesheet">
     <link href="assets/styles/style.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Oswald:700|Quattrocento" rel="stylesheet">
 
   </head>
 
-  <body class="text-center">
-
-    <div class="container-fluid d-flex w-100 h-100 p-3 mx-auto flex-column">
-
-      <header class="masthead mb-auto">
-        <div class="inner">
-          <h3 class="masthead-brand">HERITAGE EMOTIONS</h3>
-        </div>
-      </header>
-
-      <main role="main" class="inner container w-50">
-        <h1 class="cover-heading">La culture en période de guerre, une bombe à retardement</h1>
-        <h2 class="lead">En période de conflit, les tensions identitaires et culturelles se condensent et s’exacerbent. Le patrimoine culturel devient un enjeu national central. Tributaire de la propagande.</h2>
-        <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary">GET STARTED</a>
-        </p>
-      </main>
-
-      <footer class="mastfoot mt-auto">
-        <div class="inner">
-          <p></p>
-        </div>
-      </footer>
+    <div class="section no-pad-bot" id="index-banner">
+      <div class="container center">
+      <div class="row center">
+        <h2 class="header col s6 m6 l8 offset-s3 offset-m3 offset-l2">La culture en période de guerre, une bombe à retardement</h2>
+      </div>
+      <div class="row center">
+        <p class="header col s6 m6 l8 offset-s3 offset-m3 offset-l2 light">En période de conflit, les tensions identitaires et culturelles se condensent et s’exacerbent. Le patrimoine culturel devient un enjeu national central. Tributaire de la propagande.</p>
+      </div>
+      <div class="row center">
+          <a href="#" id="start-button" class="btn-large waves-effect waves-light valign-wrapper center-block">Get Started</a>
+      </div>
     </div>
+  </div>
 
-</body>
-</html>
+  <?php
+
+  include 'footer.php'
+
+  ?>
