45dbf67bae91872b4898cce63aed0bf66686df9b
Aurélien <aurelien.combes@hetic.net>
2018-06-27 11:07:32 +0200

    fix home

---
+++ views/pages/home.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png"/>
+
+    <title>HERITAGE EMOTIONS</title>
+
+    <link href="assets/styles/reset.css" rel="stylesheet">
+    <link href="vendor/materialize.min.css" rel="stylesheet">
+    <link href="assets/styles/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:700|Quattrocento" rel="stylesheet">
+
+  </head>
+
+    <div class="section no-pad-bot" id="index-banner">
+      <div class="container center">
+      <div class="row center">
+        <h2 class="header col s6 m6 l8 offset-s3 offset-m3 offset-l2">La culture en période de guerre, une bombe à retardement</h2>
+      </div>
+      <div class="row center">
+        <p class="header col s6 m6 l8 offset-s3 offset-m3 offset-l2 light">En période de conflit, les tensions identitaires et culturelles se condensent et s’exacerbent. Le patrimoine culturel devient un enjeu national central. Tributaire de la propagande.</p>
+      </div>
+      <div class="row center">
+          <a href="#" id="start-button" class="btn-large waves-effect waves-light valign-wrapper center-block">Get Started</a>
+      </div>
+    </div>
+  </div>
+
+  <?php
+
+  include 'views/partialsfooter.php';
